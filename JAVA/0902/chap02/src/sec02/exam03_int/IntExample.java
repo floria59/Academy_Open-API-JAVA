006775e170829888f7c5919f9c718283d3990faa
Butter_potato <109928969+floria59@users.noreply.github.com>
2022-09-02 17:46:54 +0900

    Add files via upload

---
+++ JAVA/0902/chap02/src/sec02/exam03_int/IntExample.java
@@ -0,0 +1,19 @@
+package sec02.exam03_int;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+		// int형 데이터타입 : 정수형데이터 타입 기본 (byte, short는 실제 사용 별로안함)
+		
+		int var1 = 10; // 10진수표현
+		int var2 = 012; // 8진수표현
+		int var3 = 0xA; // 16진수표현
+				
+	 
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+
+	}
+
+}
