57d8f4b5e6c50efb310003f49e286a22a3ff7068
Butter_potato <109928969+floria59@users.noreply.github.com>
2022-10-05 09:19:42 +0900

    Add files via upload

---
+++ JAVA/chap18/src/sec02/exam01_inputstream_read/ReadExample1.java
@@ -0,0 +1,28 @@
+package sec02.exam01_inputstream_read;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class ReadExample1 {
+
+	public static void main(String[] args) throws Exception {
+		//자원 : 파일 
+		// 내용: Hello World!!!
+		// 방향 : 입력
+		// 바이트기반 : 스트림
+		// 파일입력스트림은 데이타가 존재해야 한다.
+		InputStream is = new FileInputStream("E:/doccomsa/temp/test.txt");
+		int readByte;
+		while(true) {
+			readByte = is.read(); //한바이트씩 읽고, 앞으로 포인터(커서)위치가 이동. 포인터는 첫줄의 첫문자에 위치한다.
+			if(readByte == -1) break;
+			System.out.println((char) readByte);
+		}
+		
+		is.close();
+		
+
+	}
+
+}
